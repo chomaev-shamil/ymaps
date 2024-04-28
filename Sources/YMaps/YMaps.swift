cb24904e05b939e60974a6c4921da78fadaec5db
Shamil Chomaev <sham.chom11@gmail.com>
2024-04-28 05:25:56 +0300

    add isM1Simulator

--- Sources/YMaps/YMaps.swift
+++ Sources/YMaps/YMaps.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+#if targetEnvironment(simulator)
+public func isM1Simulator() -> Bool {
+    return TARGET_CPU_ARM64 != 0
+}
+#else
+public func isM1Simulator() -> Bool { false }
+#endif
