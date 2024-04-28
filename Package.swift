18317546e69da4f38f0fc5a060c76a9f3c0796b9
Shamil Chomaev <sham.chom11@gmail.com>
2024-04-28 05:31:22 +0300

    update ios version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "YMaps",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v16),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
