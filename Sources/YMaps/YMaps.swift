d79748eb6ea8e1ad7249ee62c52877f88fa819ba
Shamil Chomaev <sham.chom11@gmail.com>
2024-04-28 05:33:20 +0300

    upd

--- Sources/YMaps/YMaps.swift
+++ Sources/YMaps/YMaps.swift
@@ -1,10 +1,14 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//// The Swift Programming Language
+//// https://docs.swift.org/swift-book
+//
+//#if targetEnvironment(simulator)
+//public func isM1Simulator() -> Bool {
+//    return TARGET_CPU_ARM64 != 0
+//}
+//#else
+//public func isM1Simulator() -> Bool { false }
+//#endif
 
-#if targetEnvironment(simulator)
-public func isM1Simulator() -> Bool {
-    return TARGET_CPU_ARM64 != 0
+public func connectCheck() -> Bool {
+    return true
 }
-#else
-public func isM1Simulator() -> Bool { false }
-#endif
